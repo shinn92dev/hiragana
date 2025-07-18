19ae22b5af9a2e48b429232543d2af23545b0ef8
Anthony Shin <shinn92dev@gmail.com>
2025-07-18 15:40:37 -0700

    ✨ feat: implement feature that fetch highlighted key

--- components/custom/QuestFooter.tsx
+++ components/custom/QuestFooter.tsx
@@ -1,9 +1,32 @@
+import { useRouter } from "expo-router";
 import { Box } from "../ui/box";
 import { Pressable } from "../ui/pressable";
 import { StyleSheet, Text } from "react-native";
+import { getAllKanaStatus } from "@/db/db";
+import { useEffect } from "react";
+
 const QuestFooter = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const getCurrentKanaRow = async () => {
+      const status = await getAllKanaStatus();
+      if (!status) {
+        return null;
+      }
+      const highlightedKey = Object.entries(status).find(
+        ([_, value]) => value.highlight
+      )?.[0];
+      return highlightedKey;
+    };
+    console.log(getCurrentKanaRow());
+  }, []);
   return (
-    <Pressable className="w-full py-5 my-10">
+    <Pressable
+      className="w-full py-5 my-10"
+      onPress={() => {
+        // router.push("");
+      }}
+    >
       <Box className="w-4/5 bg-highlight-border" style={styles.buttonContainer}>
         <Text style={styles.text}>Quest</Text>
       </Box>
